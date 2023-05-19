2808d32b1a4cc9d73ba6393fa70163be0d73613a
RudenkoOV <kpaxx@ukr.net>
2023-05-19 18:40:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/RudenkoOV.github.io/",
+  base: "/RudenkoOV.github.io/HW4/",
   plugins: [react()],
 })
